8f28291e5c1c1009c8c34212986c832ba998f355
frajerzycki <frajerzycki@protonmail.com>
2019-02-23 20:34:20 +0100

    Works.

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 const axios = require('axios');
 const fs = require('mz/fs');
-
+const ip = 'localhost';
 setInterval(function () {
-    let temp = parseInt(fs.readFileSync('/sys/bus/w1/devices/28-0317021921ff/w1_slave').toString().split('=')[2]) / 1000
-    console.log(temp);
-    axios.post("http://192.168.1.100:5000/info", {
-            temperature: temp.toString()
+    let temp = parseInt(fs.readFileSync('/sys/bus/w1/devices/28-0317021921ff/w1_slave').toString().split('=')[2]) / 1000;
+    //let temp = 22;
+    axios.post(`http://${ip}:5000/info`, {
+            temperature: temp.toString(),
+	    date: Date.now()
         }).then(function (response) {
             console.log(response);
         })
         .catch(function (error) {
             console.log(error);
         });
-}, 1000)
+}, 1000);
