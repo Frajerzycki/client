607f1dbefadb2ba7b6021bc38e6e2db5d36348b0
Your Name <you@example.com>
2019-02-23 11:55:46 +0100

    Something added.

--- client.js
+++ client.js
@@ -1,9 +1,15 @@
 const axios = require('axios');
-axios.get('http://192.168.1.109:5000').then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
+const fs = require('mz/fs');
+
+setInterval(function () {
+    let temp = parseInt(fs.readFileSync('/sys/bus/w1/devices/28-0317021921ff/w1_slave').toString().split('=')[2]) / 1000
+    console.log(temp);
+    axios.post("http://192.168.1.100:5000/info", {
+            temperature: temp.toString()
+        }).then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}, 1000)
